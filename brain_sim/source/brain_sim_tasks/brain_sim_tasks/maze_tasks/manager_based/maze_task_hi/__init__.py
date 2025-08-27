7bc9bf8e8aed9c7616115676a864a91a351eb42f
Yihao Liu <yliu333@jhu.edu>
2025-08-27 09:50:00 -0700

    [MINOR] Added test flag on the test environments

--- brain_sim/source/brain_sim_tasks/brain_sim_tasks/maze_tasks/manager_based/maze_task_hi/__init__.py
+++ brain_sim/source/brain_sim_tasks/brain_sim_tasks/maze_tasks/manager_based/maze_task_hi/__init__.py
@@ -13,7 +13,7 @@ from . import agents
 
 
 gym.register(
-    id="Brain-Sim-Maze-High-Level-v0",
+    id="Brain-Sim-Maze-High-Level-Test-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
