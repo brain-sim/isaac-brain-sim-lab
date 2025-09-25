2180567047dde5cedcb63e846bac5a03f218b3a2
Yihao Liu <yihao.liu@astera.org>
2025-09-25 16:32:53 -0700

    [DEV] New orange

--- source/brain_sim_assets/brain_sim_assets/props/kitchen/entities/fruit.py
+++ source/brain_sim_assets/brain_sim_assets/props/kitchen/entities/fruit.py
@@ -7,6 +7,7 @@ from brain_sim_assets import BRAIN_SIM_ASSETS_PROPS_DATA_DIR
 _FRUIT_PATHS = {
     "orange1": "kitchen/Kitchen_Other/Kitchen_Orange001.usd",
     "orange2": "kitchen/Kitchen_Other/Kitchen_Orange002.usd",
+    "orange_1": "kitchen/orange_1/orange_1.usd"
 }
 
 
@@ -18,9 +19,9 @@ class bsKitchenFruit:
         pos: tuple = (0.0, 0.0, 0.0),
         rot: tuple = (1.0, 0.0, 0.0, 0.0),
         scale: tuple = (1.0, 1.0, 1.0),
-        variant: str = "orange1",
+        variant: str = "orange_1",
     ) -> AssetBaseCfg:
-        usd_rel_path = _FRUIT_PATHS.get(variant, _FRUIT_PATHS["orange1"])
+        usd_rel_path = _FRUIT_PATHS.get(variant, _FRUIT_PATHS["orange_1"])
         return AssetBaseCfg(
             prim_path=prim_path,
             init_state=AssetBaseCfg.InitialStateCfg(pos=pos, rot=rot),
